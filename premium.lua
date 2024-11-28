0f66d12fe6cadbdde2afabe00176b7bc584771bd
desty <83312843+destylol@users.noreply.github.com>
2024-11-28 19:53:39 +0100

    Create premium.lua

---
+++ premium.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 15759515082 then
+    print("king legacy - nebora")
+    return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/03309b2430d4fa4ec2a71deff82423c2.lua"))()
+elseif game.PlaceId == 16732694052 then
+    print("fisch - nebora")
+    return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/68f4ddd024c5dc0e1ff85e4008d2d667.lua"))()
+end
