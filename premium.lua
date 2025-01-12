c38ec83f0c4c98c5dcca2cb0745a59ed0f417be3
desty <83312843+destylol@users.noreply.github.com>
2025-01-12 02:10:06 +0100

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -4,4 +4,6 @@ if game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 1
 elseif game.PlaceId == 16732694052 then
     print("fisch - nebora")
     return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/03309b2430d4fa4ec2a71deff82423c2.lua"))()
+else
+    return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/370203cf97844b1690c4e596ffe31714.lua"))()
 end
