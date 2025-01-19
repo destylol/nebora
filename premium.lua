f0be8bc167160b947dccbfa33ae8885e836e63e4
desty <83312843+destylol@users.noreply.github.com>
2025-01-19 23:34:52 +0100

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -6,4 +6,6 @@ elseif game.PlaceId == 16732694052 then
     return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/03309b2430d4fa4ec2a71deff82423c2.lua"))()
 elseif game.PlaceId == 7606602544 or game.PlaceId == 7606564092 then
     return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/370203cf97844b1690c4e596ffe31714.lua"))()
+elseif game.GameId == 6035872082 then
+    return loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8984d096da6fa841a78a0d0a2a1bfe13.lua"))()
 end
